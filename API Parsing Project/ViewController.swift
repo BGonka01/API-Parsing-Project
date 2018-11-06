84c2c72af7fa6e415a3ba8cfdfe432993bb4ddf1
MIGUEL TAMAYO <rl237854@rlas-macair-32.local>
2018-11-06 12:42:18 -0600

    Initial Commit

---
+++ API Parsing Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  API Parsing Project
+//
+//  Created by MIGUEL TAMAYO on 11/6/18.
+//  Copyright © 2018 MIGUEL TAMAYO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
