f042237e7acb46fa294fe289d227eb672bb8b5ee
MIGUEL TAMAYO <rl237854@rlas-macair-32.local>
2018-12-03 12:31:14 -0600

    Cat Facts

--- API Parsing Project/ViewController.swift
+++ API Parsing Project/ViewController.swift
@@ -1,20 +1,57 @@
-//
-//  ViewController.swift
-//  API Parsing Project
-//
-//  Created by MIGUEL TAMAYO on 11/6/18.
-//  Copyright © 2018 MIGUEL TAMAYO. All rights reserved.
-//
+
 
 import UIKit
 
+struct AllFacts: Decodable {
+    var catFact: [CatFact]
+}
+
+struct CatFact: Decodable {
+    let text: String
+}
+
 class ViewController: UIViewController {
+    
+
+    @IBOutlet weak var catFactLabel: UILabel!
+    //let APIKey = String()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print()
     }
-
-
+    
+    @IBAction func onCatFactButtonTapped(_ sender: UIButton) {
+    //APIKey = ""
+     parse() }
+    
+    func parse() {
+        let jsonUrlString = "https://cat-fact.herokuapp.com"
+        guard let url = URL(string: jsonUrlString) else { return }
+        URLSession.shared.dataTask(with: url){ (data, response, err) in
+            guard let data = data else { return }
+            do {
+                let facts = try JSONDecoder().decode([CatFact].self, from: data)
+                print(facts)
+                
+                
+                
+                
+                
+                
+            } catch let jsonErr {
+                print("error serializing data:", jsonErr)
+                }
+            }.resume()
+    }
+    
+    
+    
 }
+
+
+
+
+
 
