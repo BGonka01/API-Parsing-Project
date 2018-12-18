22edd399f34a66fa1423c2777741761c3bd4e19e
BRANDEN GONKA <rl106682@rlas-macair-21.rlas-116.org>
2018-12-18 13:52:40 -0600

    pulled son data to console

--- API Parsing Project/ViewController.swift
+++ API Parsing Project/ViewController.swift
@@ -2,53 +2,84 @@
 
 import UIKit
 
-struct AllFacts: Decodable {
-    var catFact: [CatFact]
+struct All: Decodable {
+    let all: [Text]
 }
 
-struct CatFact: Decodable {
+struct Text: Codable {
     let text: String
+}
+
+enum CodingKeys: String, CodingKey {
+    case text
 }
 
 class ViewController: UIViewController {
     
-
+    let fact: String = ""
+    
     @IBOutlet weak var catFactLabel: UILabel!
     //let APIKey = String()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        print()
+        parse()
     }
     
-    @IBAction func onCatFactButtonTapped(_ sender: UIButton) {
-    //APIKey = ""
-     parse() }
+    @IBAction func CatFacts(_ sender: Any) {
+        parse()
+    }
     
     func parse() {
-        let jsonUrlString = "https://cat-fact.herokuapp.com"
-        guard let url = URL(string: jsonUrlString) else { return }
-        URLSession.shared.dataTask(with: url){ (data, response, err) in
-            guard let data = data else { return }
+        let jsonUrlString = "https://cat-fact.herokuapp.com/facts/\(fact)"
+        guard let url = URL(string: jsonUrlString) else {return}
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            guard let data = data else{ return }
             do {
-                let facts = try JSONDecoder().decode([CatFact].self, from: data)
-                print(facts)
-                
-                
-                
-                
-                
-                
-            } catch let jsonErr {
-                print("error serializing data:", jsonErr)
+                let facts = try? JSONDecoder().decode(All.self, from: data)
+                print(facts?.all)
+                for fact in facts{
+                    print(fact)
                 }
-            }.resume()
-    }
-    
-    
-    
+            }
+            //                if let jsonData = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! NSDictionary {
+            //                    print(jsonData["all"])
+            //                    for result in jsonData {
+            //                    }
+        }
+    }.resume()
 }
+
+
+func displayInfo(catfact: String){
+    DispatchQueue.main.async {
+        self.catFactLabel.text = "fact: \(catfact)"
+    }
+}
+//APIKey = ""
+//        parse()
+
+//    func parse() {
+//
+//        let jsonUrlString = "https://cat-fact.herokuapp.com/facts"
+//        guard let url = URL(string: jsonUrlString) else { return }
+//        URLSession.shared.dataTask(with: url){ (data, response, err) in
+//            guard let data = data else { return }
+//            do {
+//                if let jsonData = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as! NSDictionary {
+//                }
+//            }
+//
+//        } catch let jsonErr {
+//            print("error serializing data:", jsonErr)
+//        }
+//    }
+
+}
+
+
+
+
 
 
 
